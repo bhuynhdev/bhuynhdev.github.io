ecb83040d40f5b41f9d1b2234ad1c0d018676705
Bao Huynh Lam <shinsusenju@aol.com>
2020-12-14 10:48:51 +0700

    Add typed.js and improve button animation

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function typeName() {
+    const options = {
+        strings: ["Bao Huynh"],
+        typeSpeed: 50
+    };
+    
+    const typed = new Typed('.typing', options);
+};
+
+
+
+window.onload = function() { typeName(); };
+
